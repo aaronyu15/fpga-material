1bd7262d5dadac5b85a38b584fbc82196d946dec
aaron <aaronyu15@hotmail.com>
2026-04-20 12:15:44 -0400

    Edits

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -31,7 +31,6 @@ export const defaultContentPageLayout: PageLayout = {
           grow: true,
         },
         { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
       ],
     }),
     Component.Explorer(),
